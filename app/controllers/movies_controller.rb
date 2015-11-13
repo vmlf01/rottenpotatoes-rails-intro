5f6eb1ece92ad7180387b122e8532b90d1ea142e
Vitor Fernandes <vmlf01@gmail.com>
2015-11-13 18:45:37 +0000

    add session check for params values

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,14 +17,16 @@ class MoviesController < ApplicationController
   end
 
   def index
-    initCssClasses
-    
-    movie_query = appendSortParams(Movie, @css)
-    movie_query = appendFilterParams(movie_query)
-
-    puts "ratings: #{@ratings_to_show}"
-
-    @movies = movie_query.all
+    if !redirect_because_of_params
+      initCssClasses
+      
+      movie_query = appendSortParams(Movie, @css)
+      movie_query = appendFilterParams(movie_query)
+  
+      puts "ratings: #{@ratings_to_show}"
+  
+      @movies = movie_query.all
+    end
   end
 
   def new
@@ -64,24 +66,50 @@ class MoviesController < ApplicationController
   end
   
   def appendSortParams(movie_query, css)
-    sort_column = params[:sort]
+    sort_column = params[:sort] ||= session[:sort];
 
     if sort_column =~ /^(title|release_date)$/i
       sort_column.downcase!
       css[sort_column] = 'hilite'
       movie_query = movie_query.order(sort_column)
+      session[:sort] = params[:sort]
+    else
+      session.delete :sort
     end
     
     movie_query
   end
   
   def appendFilterParams(movie_query)
-    @ratings_to_show = [] unless params[:commit].nil?
-    @ratings_to_show = params[:ratings].keys unless params[:ratings].nil?
+    @ratings_to_show = params[:ratings] ||= session[:ratings]
 
-    movie_query = movie_query.where({ "rating" => @ratings_to_show }) unless @ratings_to_show.nil?
+    #@ratings_to_show = [] unless params[:commit].nil?
+    #@ratings_to_show = params[:ratings].keys unless params[:ratings].nil?
+    if (@ratings_to_show.nil?)
+      session.delete :ratings
+      @ratings_to_show = @all_ratings
+    else
+      @ratings_to_show = @ratings_to_show.keys
+      movie_query = movie_query.where({ "rating" => @ratings_to_show })
+      session[:ratings] = params[:ratings]
+    end
     
     movie_query
+  end
+
+  def redirect_because_of_params
+    needs_redirect = should_use_session_value?(:sort) || should_use_session_value?(:ratings)
+    if (needs_redirect)
+      params[:sort] = params[:sort] ||= session[:sort]
+      params[:ratings] = params[:ratings] ||= session[:ratings]
+  
+      flash.keep
+      redirect_to movies_path(params)
+    end
+  end
+
+  def should_use_session_value?(param)
+    params[param].nil? && !session[param].nil?
   end
 
 end
